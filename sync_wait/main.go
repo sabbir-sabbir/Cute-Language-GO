d7ff581417f21ec43b70644dd2846bb4ae6b798c
sabbir-sabbir <iosabbir7890@gmail.com>
2024-12-25 17:53:26 +0600

    implement worker function with WaitGroup for concurrent task execution

--- sync_wait/main.go
+++ sync_wait/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+
+func worker(i int, wg *sync.WaitGroup){
+	defer wg.Done() //decrement the routine after completion
+	fmt.Println("worker work started", i)
+	// some task are happening
+	fmt.Println("worker work end", i)
+	
+
+}
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("worker task main function")
+     var wg sync.WaitGroup
+	// three workers
+	for i:=1; i <= 3; i++ {
+		wg.Add(1)  //increment the ruitine
+		go worker(i, &wg)
+	}
+	wg.Wait()
+	fmt.Println("worker task completed")
 }
