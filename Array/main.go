8116cce3393cc334b0c13c29b7d77f299314eec0
sabbir-sabbir <iosabbir7890@gmail.com>
2025-02-03 20:15:04 +0600

    add

--- Array/main.go
+++ Array/main.go
@@ -3,19 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Println("Hello, World!, Now we are trying to arrays")
-	// var name[5] string
-	// name[0] = "Hello"
-	// name[1] = "World"
-	// fmt.Println(name[0], name[1])
+	fmt.Println("This is Array")
+	var name[5] string
+	name[0] = "Hello"
+	name[1] = "World"
+	fmt.Println(name[0], name[1])
 
-	// name := [3]string{"Hello", "World", "Go"}
-	// fmt.Println(name[0], name[1], name[2])
-
-	var name  [5]string
-
-	name[0] = "Sabbir"
-	name[4] = "Rahman"
-
-	fmt.Printf("Show the array: %q\n", name)
+	number := [7]int{1, 2, 3, 4, 5}
+	fmt.Println(number)
+	fmt.Println(len(number))
+	fmt.Println("_____________________________________________________")
+	fmt.Println(name[1])
+    price := [5]int{}
+	fmt.Println(price)
+	var numbers[50]string
+	fmt.Println(numbers)
+	fmt.Printf( "%q", numbers)
 }
--- slice/main.go
+++ slice/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!, Now we are trying to slice")
+	fmt.Println("Starting the slice...")
+	number := []int{1, 2, 3, 4,5,6}
+	fmt.Println(number)
+	number = append(number, 10,10,10,10,100,200,500,800,600,300)
+	fmt.Println(number)
+    fmt.Printf("Length of numbers now: %d\n", len(number))
 }
