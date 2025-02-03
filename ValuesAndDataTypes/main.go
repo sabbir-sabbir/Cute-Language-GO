3bae8008aab711c8abbbd807a8f698af8c9d33fc
sabbir-sabbir <iosabbir7890@gmail.com>
2025-02-03 15:18:38 +0600

    update

---
+++ ValuesAndDataTypes/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)func main() {
+	str1 := "sabbir"
+	str2 := "rahman"
+	// result := strings.Join([]string{str1, str2}, " + ")
+	result := strings.Join([]string{str1, str2}, " ")
+	fmt.Println(result)
+
+
+}
