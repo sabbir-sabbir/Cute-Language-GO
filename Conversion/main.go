ff29f1f8ec7f2e448fa8af5127a0b95b1521dff2
sabbir-sabbir <iosabbir7890@gmail.com>
2025-02-04 23:36:17 +0600

    add

--- Conversion/main.go
+++ Conversion/main.go
@@ -6,10 +6,16 @@ import (
 )
 
 func main() {
-	var num = 50
-	str := strconv.Itoa(num)
+	// var num = 50
+	// str := strconv.Itoa(num)
 
-	fmt.Println("String: ", str)
-	fmt.Printf("Type: %T\n", str)
+	// fmt.Println("String: ", str)
+	// fmt.Printf("Type: %T\n", str)
+
+	number_str := "12345"
+
+	str_num, _ := strconv.Atoi(number_str)
+	fmt.Println(str_num)
+	fmt.Printf("Type of this value %T", str_num)
 
 }
